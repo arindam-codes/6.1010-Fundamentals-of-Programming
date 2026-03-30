a24e297c5ba1ebed87649624f865d2a7129b22f6
Arindam B <arindamcodes394@gmail.com>
2026-03-30 14:35:20 +0530

    debugged using basics

--- week0/debugging_starter.py
+++ week0/debugging_starter.py
@@ -1,24 +1,24 @@
-# def sum_lists(lists):
-#     """
-#     Given a list of lists, return a new list where each list is replaced by
-#     the sum of its elements.
-#     """
-#     output = [0] * len(lists)
-#     print("heres i'm -->", output, len(lists))
-#     for i in range(len(lists)):
-#         inner_list = lists[i]
-#         print("heres the list -->", inner_list, i)
-#         total = 0
-#         for j in inner_list:
-#             print("heres j -->", j)
-#             total += j
-#         print("heres total -->", total)
-#         output[i] = total
-#         print("heres i'm -->", output)
-#     return output
+def sum_lists(lists):
+    """
+    Given a list of lists, return a new list where each list is replaced by
+    the sum of its elements.
+    """
+    output = [0] * len(lists)
+    print("heres i'm -->", output, len(lists))
+    for i in range(len(lists)):
+        inner_list = lists[i]
+        print("heres the list -->", inner_list, i)
+        total = 0
+        for j in inner_list:
+            print("heres j -->", j)
+            total += j
+        print("heres total -->", total)
+        output[i] = total
+        print("heres i'm -->", output)
+    return output
 
 
-# print(sum_lists([[5, 6], [3, 2]]))   # [11, 5]
+print(sum_lists([[5, 6], [3, 2]]))   # [11, 5]
 
 ## here i first noticed whatshappening by printing each line i noticed its stopped at ths line output[i] = total
 # then i noticed the oouter loop variable i but i get onfused with other langages like c, java, luau where scope acts like block
@@ -32,22 +32,26 @@ def reverse_all(inp):
     given a list of lists, return a new list of lists but with all of the inner
     lists reversed, without modifying the input list
 
-    # Example:
+    Example:
 
-    # >>> input1 = [[1, 2], [3, 4]]
-    # >>> reverse_all(input1)
-    # [[2, 1], [4, 3]]
-    # """
+    >>> input1 = [[1, 2], [3, 4]]
+    >>> reverse_all(input1)
+    [[2, 1], [4, 3]]
+    """
     output_copy = []
     # for i in inp
     for i in inp:
-        output_copy = output_copy + [i[:]]
+        output_copy += [i[::-1]]   # used bcz mit in website material exclusively told this line
+                                   # "This gives us yet another cute shorthand, x[::-1], 
+                                   #  which makes a new list with all of the references from x but in the reverse order."
 
-    output_copy[0][0] = 255
-    print("input list", inp)
-    #print("new list", new_list)
     return output_copy
     
 
 print("output list", reverse_all([[1, 2], [3, 4]]))
 
+
+## i[::-1] this is the life saver but still its just a shalow copy with 1 layer of depth
+# but with deepcopy or recursion based we can solve this problem 
+# its like new_list = copy.deepcopy(original_list)
+# and then we can reverse it 
